f627afd2cda66e3513906ce636111ea2236c5e34
notEvil <a_rappold@gmx.at>
2017-02-24 19:17:35 +0100

    updated parasail setup script

--- smith_waterman/setup_parasail.sh
+++ smith_waterman/setup_parasail.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -ev
-git clone https://github.com/jeffdaily/parasail.git
-mv parasail parasail-c
-cd parasail-c
+if [ -d "./parasail-c" ]; then
+    git -C "./parasail-c" pull
+else
+    git clone https://github.com/jeffdaily/parasail.git
+    mv parasail parasail-c
+fi
+pushd parasail-c
 ./configure
 make -j $(nproc --all)
-cd ..
-git clone https://github.com/jeffdaily/parasail-python.git
+popd
+if [ -d "./parasail-python" ]; then
+    git -C "./parasail-python" pull
+else
+    git clone https://github.com/jeffdaily/parasail-python.git
+fi
+mv -b parasail parasail.bak || true
 ln -s parasail-python/parasail parasail
-cd parasail-python/parasail
+pushd parasail-python/parasail
+mv -b libparasail.so libparasail.so.bak || true
 ln -s ../../parasail-c/.libs/libparasail.so.2.0.0 libparasail.so
-cd ../..
+popd
